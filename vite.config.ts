e8b0935537c10470630319d33ac287eac0bf9533
felipinhosilvajf-debug <felipinhosilvajf@gmail.com>
2026-08-10 00:11:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração para publicar no GitHub Pages em subpasta
 export default defineConfig({
   plugins: [react()],
-  base: '/mascrypto/',
+  base: process.env.NODE_ENV === 'production' ? '/mascrypto/' : '/',
 })
