3e843dfdf9c274cdbe32e241103c1dd110c94ded
felipinhosilvajf-debug <felipinhosilvajf@gmail.com>
2026-08-09 23:50:20 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mascrypto/',
+  base: './', // O './' garante que todos os arquivos (CSS/JS) usem caminhos relativos
 })
